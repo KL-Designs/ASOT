732d5d086f3cc93dbfc0e83be6cdaaa55b1e7761
Koda <koda@horizons.gg>
2026-05-27 00:16:20 +1000

    Improved DB code

--- lib/mongo.ts
+++ lib/mongo.ts
@@ -1,64 +1,64 @@
-import { MongoClient, Collection as MongoCollection } from 'mongodb'
+import { MongoClient, Collection as MongoCollection, Document } from 'mongodb'
 
+if (!process.env.MONGO_URI) throw new Error('MONGO_URI is not set')
+if (!process.env.MONGO_DB) throw new Error('MONGO_DB is not set')
 
-const client = new MongoClient(process.env.MONGO_URI!)
-client.connect().catch(console.error)
+// Cache the client on global so Next.js HMR doesn't leak a new connection on every hot reload
+const globalWithMongo = global as typeof globalThis & { _mongoClient?: MongoClient }
 
-client.on('connectionReady', () => { }) //console.info(`MongoDB Connected with "${process.env.MONGO_URI!}" using "${process.env.MONGO_DB!}"`)
+if (!globalWithMongo._mongoClient) {
+    globalWithMongo._mongoClient = new MongoClient(process.env.MONGO_URI)
+    globalWithMongo._mongoClient.connect().catch(console.error)
+}
 
+const client = globalWithMongo._mongoClient
+const db = client.db(process.env.MONGO_DB)
 
 const DbInterface = {
-    stats: () => client.db(process.env.MONGO_DB!).stats().then(console.table).catch(console.error),
+    stats: () => db.stats().then(console.table).catch(console.error),
 
-    users: client.db(process.env.MONGO_DB!).collection('users') as MongoCollection<User>,
-    roles: client.db(process.env.MONGO_DB!).collection('roles') as MongoCollection<Role>,
-    milpacs: client.db(process.env.MONGO_DB!).collection('milpacs') as MongoCollection<Milpac>,
-    optionals: client.db(process.env.MONGO_DB!).collection('optionals') as MongoCollection<Optional>,
-    operations: client.db(process.env.MONGO_DB!).collection('operations') as MongoCollection<Operation>,
-    operationActivity: client.db(process.env.MONGO_DB!).collection<OperationActivityLog>('operation_activity'),
-    minigameScores: client.db(process.env.MONGO_DB!).collection('minigame_scores'),
-    minigameLive:   client.db(process.env.MONGO_DB!).collection('minigame_live'),
-    orbatPositions: client.db(process.env.MONGO_DB!).collection('orbat_positions') as MongoCollection<OrbatPosition>,
-    orbatSectionMeta: client.db(process.env.MONGO_DB!).collection('orbat_section_meta') as MongoCollection<OrbatSectionMeta>,
-    operationAttendance: client.db(process.env.MONGO_DB!).collection('operation_attendance') as MongoCollection<OperationAttendance>,
-    j1Applications: client.db(process.env.MONGO_DB!).collection('j1_applications') as MongoCollection<J1Application>,
-    tickets: client.db(process.env.MONGO_DB!).collection('tickets') as MongoCollection<Ticket>,
-    calendarEvents: client.db(process.env.MONGO_DB!).collection('calendar_events') as MongoCollection<CalendarEvent>,
-    siteSettings: client.db(process.env.MONGO_DB!).collection('site_settings') as MongoCollection<{ _id: string } & Record<string, unknown>>,
-    operationTemplates: client.db(process.env.MONGO_DB!).collection('operation_templates') as MongoCollection<OperationTemplate>,
-    operationCampaigns: client.db(process.env.MONGO_DB!).collection('operation_campaigns') as MongoCollection<OperationCampaign>,
-    campaignMissions: client.db(process.env.MONGO_DB!).collection('campaign_missions') as any,
-    notifications: client.db(process.env.MONGO_DB!).collection('notifications') as MongoCollection<Notification>,
-    tasks: client.db(process.env.MONGO_DB!).collection('tasks') as MongoCollection<Task>,
-    calendarReminders: client.db(process.env.MONGO_DB!).collection('calendar_reminders') as MongoCollection<CalendarReminder>,
-    meetings: client.db(process.env.MONGO_DB!).collection('meetings') as MongoCollection<Meeting>,
-    actionLogs: client.db(process.env.MONGO_DB!).collection('action_logs') as MongoCollection<ActionLog>,
-    errorLogs: client.db(process.env.MONGO_DB!).collection('error_logs') as MongoCollection<ErrorLog>,
-    discordLogs: client.db(process.env.MONGO_DB!).collection('discord_logs') as MongoCollection<DiscordLog>,
-    driversLicense: client.db(process.env.MONGO_DB!).collection('drivers_license') as MongoCollection<DriverLicenseEntry>,
-    mapPresets: client.db(process.env.MONGO_DB!).collection('map_presets'),
-    retiredMembers: client.db(process.env.MONGO_DB!).collection('retired_members') as MongoCollection<RetiredMember>,
-    quizAttempts: client.db(process.env.MONGO_DB!).collection('quiz_attempts') as MongoCollection<QuizAttempt>,
-    communityTickets: client.db(process.env.MONGO_DB!).collection('feedback') as MongoCollection<CommunityTicket>,
-    communityTicketComments: client.db(process.env.MONGO_DB!).collection('feedback_comments') as MongoCollection<CommunityTicketComment>,
-    meetingNotifQueue: client.db(process.env.MONGO_DB!).collection('meeting_notif_queue') as MongoCollection<MeetingNotifQueueRecord>,
-    userPreferences: client.db(process.env.MONGO_DB!).collection('user_preferences') as MongoCollection<UserPreferences>,
-    notifPolicyConfig: client.db(process.env.MONGO_DB!).collection('notif_policy_config') as MongoCollection<NotifPolicyConfig>,
-    sops: client.db(process.env.MONGO_DB!).collection('sops') as MongoCollection<SopDocument>,
-    teamspeakSnapshots: client.db(process.env.MONGO_DB!).collection('teamspeak_snapshots') as MongoCollection<TsSnapshot>,
-    recruitSessions: client.db(process.env.MONGO_DB!).collection('recruit_sessions') as MongoCollection<RecruitSession>,
-    tfarPlugins: client.db(process.env.MONGO_DB!).collection('tfar_plugins'),
-    inProgressRecruitments: client.db(process.env.MONGO_DB!).collection('in_progress_recruitments') as any,
-    workspaceFiles: client.db(process.env.MONGO_DB!).collection('workspace_files') as any,
-    workspaceDocs: client.db(process.env.MONGO_DB!).collection('workspace_docs') as any,
-    workspaceVersions: client.db(process.env.MONGO_DB!).collection('workspace_versions') as any,
-
-    // ranks: client.db(process.env.MONGO_DB!).collection('ranks') as MongoCollection<Rank>,
-    // roles: client.db(process.env.MONGO_DB!).collection('roles') as MongoCollection<Role>,
-    // sections: client.db(process.env.MONGO_DB!).collection('sections') as MongoCollection<Section>,
-    // platoons: client.db(process.env.MONGO_DB!).collection('platoons') as MongoCollection<Platoon>,
-    // certifications: client.db(process.env.MONGO_DB!).collection('certifications') as MongoCollection<Certification>,
-    // awards: client.db(process.env.MONGO_DB!).collection('awards') as MongoCollection<Award>,
+    users: db.collection('users') as MongoCollection<User>,
+    roles: db.collection('roles') as MongoCollection<Role>,
+    milpacs: db.collection('milpacs') as MongoCollection<Milpac>,
+    optionals: db.collection('optionals') as MongoCollection<Optional>,
+    operations: db.collection('operations') as MongoCollection<Operation>,
+    operationActivity: db.collection<OperationActivityLog>('operation_activity'),
+    minigameScores: db.collection('minigame_scores'),
+    minigameLive: db.collection('minigame_live'),
+    orbatPositions: db.collection('orbat_positions') as MongoCollection<OrbatPosition>,
+    orbatSectionMeta: db.collection('orbat_section_meta') as MongoCollection<OrbatSectionMeta>,
+    operationAttendance: db.collection('operation_attendance') as MongoCollection<OperationAttendance>,
+    j1Applications: db.collection('j1_applications') as MongoCollection<J1Application>,
+    tickets: db.collection('tickets') as MongoCollection<Ticket>,
+    calendarEvents: db.collection('calendar_events') as MongoCollection<CalendarEvent>,
+    siteSettings: db.collection('site_settings') as MongoCollection<{ _id: string } & Record<string, unknown>>,
+    operationTemplates: db.collection('operation_templates') as MongoCollection<OperationTemplate>,
+    operationCampaigns: db.collection('operation_campaigns') as MongoCollection<OperationCampaign>,
+    campaignMissions: db.collection('campaign_missions') as MongoCollection<CampaignMission>,
+    notifications: db.collection('notifications') as MongoCollection<Notification>,
+    tasks: db.collection('tasks') as MongoCollection<Task>,
+    calendarReminders: db.collection('calendar_reminders') as MongoCollection<CalendarReminder>,
+    meetings: db.collection('meetings') as MongoCollection<Meeting>,
+    actionLogs: db.collection('action_logs') as MongoCollection<ActionLog>,
+    errorLogs: db.collection('error_logs') as MongoCollection<ErrorLog>,
+    discordLogs: db.collection('discord_logs') as MongoCollection<DiscordLog>,
+    driversLicense: db.collection('drivers_license') as MongoCollection<DriverLicenseEntry>,
+    mapPresets: db.collection('map_presets'),
+    retiredMembers: db.collection('retired_members') as MongoCollection<RetiredMember>,
+    quizAttempts: db.collection('quiz_attempts') as MongoCollection<QuizAttempt>,
+    communityTickets: db.collection('feedback') as MongoCollection<CommunityTicket>,
+    communityTicketComments: db.collection('feedback_comments') as MongoCollection<CommunityTicketComment>,
+    meetingNotifQueue: db.collection('meeting_notif_queue') as MongoCollection<MeetingNotifQueueRecord>,
+    userPreferences: db.collection('user_preferences') as MongoCollection<UserPreferences>,
+    notifPolicyConfig: db.collection('notif_policy_config') as MongoCollection<NotifPolicyConfig>,
+    sops: db.collection('sops') as MongoCollection<SopDocument>,
+    teamspeakSnapshots: db.collection('teamspeak_snapshots') as MongoCollection<TsSnapshot>,
+    recruitSessions: db.collection('recruit_sessions') as MongoCollection<RecruitSession>,
+    tfarPlugins: db.collection('tfar_plugins'),
+    inProgressRecruitments: db.collection('in_progress_recruitments') as MongoCollection<Document>,
+    workspaceFiles: db.collection('workspace_files') as MongoCollection<Document>,
+    workspaceDocs: db.collection('workspace_docs') as MongoCollection<Document>,
+    workspaceVersions: db.collection('workspace_versions') as MongoCollection<Document>,
 }
 
 export default DbInterface
