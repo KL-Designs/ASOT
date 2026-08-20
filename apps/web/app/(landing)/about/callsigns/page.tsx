2bd47180ec4cdb60d7c2a72afb1ab33db21e4abe
Koda <koda@horizons.gg>
2026-08-21 06:15:38 +1000

    feat(about): restyle callsigns and contact
    Both are collections of like things, so both keep their cards. The channel
    accent moves onto a --acc custom property instead of five hardcoded prop
    pairs.

--- apps/web/app/(landing)/about/callsigns/page.tsx
+++ apps/web/app/(landing)/about/callsigns/page.tsx
@@ -1,8 +1,9 @@
 import { Metadata } from "next"
 
-import { Typography } from '@mui/material'
-
 import { CallsignCard } from '@/components/callsign-card'
+import List from '@/components/ui/List'
+import SectionHead from '@/components/ui/SectionHead'
+import s from '@/styles/shell.module.css'
 
 import Image_0A from '@/public/images/home/callsigns/0A.jpg'
 import Image_10 from '@/public/images/home/callsigns/10.jpg'
@@ -28,126 +29,137 @@ export const metadata: Metadata = {
 
 export default function Tab() {
 	return (
-		<div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4'>
+		<section>
+			<SectionHead kicker='Registry' title='Callsigns' />
+			<div className={s.csGrid}>
 
-			<CallsignCard title='India 0A' images={[Image_0A]}>
-				<Typography>India 0A is the commanding officer and unit owner callsign that oversees management and operation of the entire unit.</Typography>
-				<Typography>- Overall command of all assets and call signs in game.</Typography>
-				<Typography>- Admin related to the community. (Mods, documentation and development)</Typography>
-				<Typography>- Management of all group departments and staff.</Typography>
-				<Typography>- Oversees all staff and unit management.</Typography>
-				<br />
-				<Typography>If not operating on the ground as a HQ element, 0A assists call signs by filling empty staff slots for missions, helping out Zeus's when required and then simply filling empty spots in other call signs.</Typography>
-				<br />
-				<Typography>0A is comprised of the commanding officer.</Typography>
-			</CallsignCard>
+				<CallsignCard title='India 0A' images={[Image_0A]}>
+					<p>India 0A is the commanding officer and unit owner callsign that oversees management and operation of the entire unit.</p>
+					<List items={[
+						'Overall command of all assets and call signs in game.',
+						'Admin related to the community. (Mods, documentation and development)',
+						'Management of all group departments and staff.',
+						'Oversees all staff and unit management.',
+					]} />
+					<p>If not operating on the ground as a HQ element, 0A assists call signs by filling empty staff slots for missions, helping out Zeus's when required and then simply filling empty spots in other call signs.</p>
+					<p>0A is comprised of the commanding officer.</p>
+				</CallsignCard>
 
-			<CallsignCard title='India 1-0' images={[Image_10]}>
-				<Typography>India 1-0 is the lead call sign and HQ of the group that manages all other call signs both in and out of game.</Typography>
-				<Typography>- Majority of command of all assets and call signs in game.</Typography>
-				<Typography>- Admin related to the community. (Mods, documentation and development)</Typography>
-				<Typography>- Management of all group departments and staff.</Typography>
-				<br />
-				<Typography>If not operating on the ground as a HQ element, 1-0 assists call signs by filling empty staff slots for missions, helping out Zeus's when required and then simply filling empty spots in other call signs.</Typography>
-				<br />
-				<Typography>1-0 is comprised of a 4 man team which includes the company XO, ADJ, RSM and CSM.</Typography>
-			</CallsignCard>
+				<CallsignCard title='India 1-0' images={[Image_10]}>
+					<p>India 1-0 is the lead call sign and HQ of the group that manages all other call signs both in and out of game.</p>
+					<List items={[
+						'Majority of command of all assets and call signs in game.',
+						'Admin related to the community. (Mods, documentation and development)',
+						'Management of all group departments and staff.',
+					]} />
+					<p>If not operating on the ground as a HQ element, 1-0 assists call signs by filling empty staff slots for missions, helping out Zeus's when required and then simply filling empty spots in other call signs.</p>
+					<p>1-0 is comprised of a 4 man team which includes the company XO, ADJ, RSM and CSM.</p>
+				</CallsignCard>
 
-			<CallsignCard title='1-0 Zulu / Game Masters' images={[Image_GM]}>
-				<Typography>Game masters are our Zeus team who provide a capability that is critical to the running and experience of our missions.</Typography>
-				<Typography>- Control and placement of enemy, civilian and independent forces for the duration of the mission.</Typography>
-				<Typography>- Setting up of objectives and events relating to the missions story line.</Typography>
-				<Typography>- Ensuring members experience a well-balanced, active and enjoyable mission.</Typography>
-				<Typography>- Maintaining and adjusting mission flow to ensure a steady pace, preventing long periods of inactivity while keeping the mission challenging from start to finish.</Typography>
-				<br />
-				<Typography>Game Masters is a team comprised of 6 permanent members and a pool of part time members.</Typography>
-			</CallsignCard>
+				<CallsignCard title='1-0 Zulu / Game Masters' images={[Image_GM]}>
+					<p>Game masters are our Zeus team who provide a capability that is critical to the running and experience of our missions.</p>
+					<List items={[
+						'Control and placement of enemy, civilian and independent forces for the duration of the mission.',
+						'Setting up of objectives and events relating to the missions story line.',
+						'Ensuring members experience a well-balanced, active and enjoyable mission.',
+						'Maintaining and adjusting mission flow to ensure a steady pace, preventing long periods of inactivity while keeping the mission challenging from start to finish.',
+					]} />
+					<p>Game Masters is a team comprised of 6 permanent members and a pool of part time members.</p>
+				</CallsignCard>
 
-			<CallsignCard title='India 1-1' images={[Image_11]}>
-				<Typography>India 1-1 is one of our infantry platoons which are the main fighting force of the unit.</Typography>
-				<Typography>- Providing the main fighting capability to the task force.</Typography>
-				<Typography>- Utilising a variety weapons, vehicles and equipment to provide a diverse capability to the unit.</Typography>
-				<br />
-				<Typography>1-1 is a platoon comprising of 3 x 8 man sections and a 4 man platoon headquarters.</Typography>
-			</CallsignCard>
+				<CallsignCard title='India 1-1' images={[Image_11]}>
+					<p>India 1-1 is one of our infantry platoons which are the main fighting force of the unit.</p>
+					<List items={[
+						'Providing the main fighting capability to the task force.',
+						'Utilising a variety weapons, vehicles and equipment to provide a diverse capability to the unit.',
+					]} />
+					<p>1-1 is a platoon comprising of 3 x 8 man sections and a 4 man platoon headquarters.</p>
+				</CallsignCard>
 
-			<CallsignCard title='India 1-2' images={[Image_12]}>
-				<Typography>India 1-2 is one of our infantry platoons which are the main fighting force of the unit.</Typography>
-				<Typography>- Providing the main fighting capability to the task force.</Typography>
-				<Typography>- Utilising a variety weapons, vehicles and equipment to provide a diverse capability to the unit.</Typography>
-				<br />
-				<Typography>1-2 is a platoon comprising of 3 x 8 man sections and a 4 man platoon headquarters.</Typography>
-			</CallsignCard>
+				<CallsignCard title='India 1-2' images={[Image_12]}>
+					<p>India 1-2 is one of our infantry platoons which are the main fighting force of the unit.</p>
+					<List items={[
+						'Providing the main fighting capability to the task force.',
+						'Utilising a variety weapons, vehicles and equipment to provide a diverse capability to the unit.',
+					]} />
+					<p>1-2 is a platoon comprising of 3 x 8 man sections and a 4 man platoon headquarters.</p>
+				</CallsignCard>
 
-			<CallsignCard title='India 1-3-0' images={[Image_130]}>
-				<Typography>India 1-3-0 is the HQ element of the 1-3 support platoon.</Typography>
-				<Typography>- Command of all assets in 1-3 both in and out of game.</Typography>
-				<Typography>- Admin related to 1-3 platoon.</Typography>
-				<br />
-				<Typography>If not operating on the ground as a HQ element, 1-3 HQ assists call signs by filling empty 1-3 slots for missions and filling empty slots in other call signs.</Typography>
-				<br />
-				<Typography>1-3-0 is comprised of a 5 man section including the Group Captain, Troop Commander, Battery Commander, Sapper Sergeant and a Medical Sergeant.</Typography>
-			</CallsignCard>
+				<CallsignCard title='India 1-3-0' images={[Image_130]}>
+					<p>India 1-3-0 is the HQ element of the 1-3 support platoon.</p>
+					<List items={[
+						'Command of all assets in 1-3 both in and out of game.',
+						'Admin related to 1-3 platoon.',
+					]} />
+					<p>If not operating on the ground as a HQ element, 1-3 HQ assists call signs by filling empty 1-3 slots for missions and filling empty slots in other call signs.</p>
+					<p>1-3-0 is comprised of a 5 man section including the Group Captain, Troop Commander, Battery Commander, Sapper Sergeant and a Medical Sergeant.</p>
+				</CallsignCard>
 
-			<CallsignCard title='1-3 Echo' images={[Image_13E]}>
-				<Typography>1-3 Echo is our combat engineers asset that provides the unit with a wide variety of capabilities.</Typography>
-				<Typography>- Providing explosive detection, disposal and demolitions.</Typography>
-				<Typography>- Providing CBRN protection equipment and decontamination.</Typography>
-				<Typography>- Constructing FOB's, defenses and other required structures.</Typography>
-				<Typography>- Providing ground based logistical support (Repair, Refuel, Rearm)</Typography>
-				<Typography>- Managing and providing transport for prisoners, high value targets and VIP's.</Typography>
-				<br />
-				<Typography>Echo is comprised of 2x 5 man sections.</Typography>
-			</CallsignCard>
+				<CallsignCard title='1-3 Echo' images={[Image_13E]}>
+					<p>1-3 Echo is our combat engineers asset that provides the unit with a wide variety of capabilities.</p>
+					<List items={[
+						'Providing explosive detection, disposal and demolitions.',
+						'Providing CBRN protection equipment and decontamination.',
+						"Constructing FOB's, defenses and other required structures.",
+						'Providing ground based logistical support (Repair, Refuel, Rearm)',
+						"Managing and providing transport for prisoners, high value targets and VIP's.",
+					]} />
+					<p>Echo is comprised of 2x 5 man sections.</p>
+				</CallsignCard>
 
-			<CallsignCard title='1-3 Golf' images={[Image_13G]}>
-				<Typography>1-3 Golf is our indirect fire (IDF) and direct fire support weapons (DFSW) capability while allowing us having AA and IDF Detection for defensive postures.</Typography>
-				<Typography>- Supporting ground forces with the use of indirect fires including mortars, fixed and mobile artillery.</Typography>
-				<Typography>- Providing an increased anti-tank and anti-air capability with the use of mobile launchers.</Typography>
-				<Typography>- Supporting ground forces with the use of direct fires including HMG, MMG and GMG static weaponry.</Typography>
-				<br />
-				<Typography>Golf is comprised of a 6 man section.</Typography>
-			</CallsignCard>
+				<CallsignCard title='1-3 Golf' images={[Image_13G]}>
+					<p>1-3 Golf is our indirect fire (IDF) and direct fire support weapons (DFSW) capability while allowing us having AA and IDF Detection for defensive postures.</p>
+					<List items={[
+						'Supporting ground forces with the use of indirect fires including mortars, fixed and mobile artillery.',
+						'Providing an increased anti-tank and anti-air capability with the use of mobile launchers.',
+						'Supporting ground forces with the use of direct fires including HMG, MMG and GMG static weaponry.',
+					]} />
+					<p>Golf is comprised of a 6 man section.</p>
+				</CallsignCard>
 
-			<CallsignCard title='1-3 Hotel' images={[Image_13H2]}>
-				<Typography>1-3 Hotel is our rotary air support wing / asset that provides the task force with a variety of logistical and close air support capability.</Typography>
-				<Typography>- Providing air lift capability of personnel and vehicles into the area of operations.</Typography>
-				<Typography>- Providing an air logistical capability by conducting supply drops.</Typography>
-				<Typography>- Providing battlefield commentary to FO's and Command via observation.</Typography>
-				<Typography>- Providing close air support to ground units.</Typography>
-				<br />
-				<Typography>Hotel is comprised of 2x 6 man flights.</Typography>
-			</CallsignCard>
+				<CallsignCard title='1-3 Hotel' images={[Image_13H2]}>
+					<p>1-3 Hotel is our rotary air support wing / asset that provides the task force with a variety of logistical and close air support capability.</p>
+					<List items={[
+						'Providing air lift capability of personnel and vehicles into the area of operations.',
+						'Providing an air logistical capability by conducting supply drops.',
+						"Providing battlefield commentary to FO's and Command via observation.",
+						'Providing close air support to ground units.',
+					]} />
+					<p>Hotel is comprised of 2x 6 man flights.</p>
+				</CallsignCard>
 
-			<CallsignCard title='1-3 Mike' images={[Image_13M]}>
-				<Typography>1-3 Mike is our medical emergency response team (MERT) who provide rapid and increased medical support to mass casualty incidents and other medical situations.</Typography>
-				<Typography>- Providing all call signs with a rapid responding and effective medical team when a call sign cannot manage it independently.</Typography>
-				<Typography>- Providing resupply of medical equipment and supplies to call signs.</Typography>
-				<Typography>- Conducting handling and backloading of severely wounded and/or deceased members, civilians, HVT's and VIP's.</Typography>
-				<br />
-				<Typography>Mike is comprised of 2x 4 man sections.</Typography>
-			</CallsignCard>
+				<CallsignCard title='1-3 Mike' images={[Image_13M]}>
+					<p>1-3 Mike is our medical emergency response team (MERT) who provide rapid and increased medical support to mass casualty incidents and other medical situations.</p>
+					<List items={[
+						'Providing all call signs with a rapid responding and effective medical team when a call sign cannot manage it independently.',
+						'Providing resupply of medical equipment and supplies to call signs.',
+						"Conducting handling and backloading of severely wounded and/or deceased members, civilians, HVT's and VIP's.",
+					]} />
+					<p>Mike is comprised of 2x 4 man sections.</p>
+				</CallsignCard>
 
-			<CallsignCard title='1-3 Victor' images={[Image_13V]}>
-				<Typography>Victor 1-3 is the cavalry and armoured support call sign for the company.</Typography>
-				<Typography>- Providing heavy firepower to call signs and increasing the company's anti-vehicle capability.</Typography>
-				<Typography>- Providing reconnaissance capability to headquarters and other call signs.</Typography>
-				<Typography>- Providing armoured transport capability to dismounted units.</Typography>
-				<br />
-				<Typography>The Victor call sign usually operates in a mounted role utilising APC's, tanks, IFV's and other vehicles, but crews can dismount and operate as teams when required.</Typography>
-				<br />
-				<Typography>Victor is comprised of 4x 3 man crews.</Typography>
-			</CallsignCard>
+				<CallsignCard title='1-3 Victor' images={[Image_13V]}>
+					<p>Victor 1-3 is the cavalry and armoured support call sign for the company.</p>
+					<List items={[
+						"Providing heavy firepower to call signs and increasing the company's anti-vehicle capability.",
+						'Providing reconnaissance capability to headquarters and other call signs.',
+						'Providing armoured transport capability to dismounted units.',
+					]} />
+					<p>The Victor call sign usually operates in a mounted role utilising APC's, tanks, IFV's and other vehicles, but crews can dismount and operate as teams when required.</p>
+					<p>Victor is comprised of 4x 3 man crews.</p>
+				</CallsignCard>
 
-			<CallsignCard title='Reservists' images={[Image_R]}>
-				<Typography>Reservists are our members who are awaiting a position to open in another call sign or who cannot commit to the expected attendance requirements.</Typography>
-				<Typography>- Filling in positions where members may be away for a Sunday mission.</Typography>
-				<Typography>- Providing the ability to bolster call sign strengths when required for particular missions.</Typography>
-				<Typography>- Allowing members to maintain a lower attendance rate due to real life commitments or events whilst still being able to be apart of the community.</Typography>
-				<br />
-				<Typography>We have a flexible number of reservist slots.</Typography>
-			</CallsignCard>
+				<CallsignCard title='Reservists' images={[Image_R]}>
+					<p>Reservists are our members who are awaiting a position to open in another call sign or who cannot commit to the expected attendance requirements.</p>
+					<List items={[
+						'Filling in positions where members may be away for a Sunday mission.',
+						'Providing the ability to bolster call sign strengths when required for particular missions.',
+						'Allowing members to maintain a lower attendance rate due to real life commitments or events whilst still being able to be apart of the community.',
+					]} />
+					<p>We have a flexible number of reservist slots.</p>
+				</CallsignCard>
 
-		</div>
+			</div>
+		</section>
 	)
 }
--- apps/web/app/(landing)/about/contact/page.tsx
+++ apps/web/app/(landing)/about/contact/page.tsx
@@ -1,120 +1,86 @@
-import { Metadata } from "next"
+import { Metadata } from 'next'
 import Link from 'next/link'
 import React from 'react'
 
-import { HeadsetMic, Groups, Email as EmailIcon, Forum, Launch } from '@mui/icons-material'
-
+import SectionHead from '@/components/ui/SectionHead'
+import s from '@/styles/shell.module.css'
 
 export const metadata: Metadata = {
 	title: "Contact | Australian Special Operations Taskforce",
 	description: "Get in touch with the Australian Special Operations Taskforce leadership and staff team.",
 }
 
-
-function ContactCard({ icon, title, accentColor, accentRgb, href, label, description, external }: {
-	icon: React.ReactNode
+function Channel({ mark, title, accent, href, label, description, external }: {
+	mark: string
 	title: string
-	accentColor: string
-	accentRgb: string
+	accent: string
 	href: string
 	label: string
 	description: string
 	external?: boolean
 }) {
 	return (
-		<Link href={href as any} target={external ? '_blank' : '_self'}>
-			<div
-				className='h-full flex flex-col gap-4 p-5 cursor-pointer transition-all duration-200 hover:bg-white/[0.04]'
-				style={{
-					border: '1px solid rgba(255,255,255,0.07)',
-					borderTop: `2px solid ${accentColor}`,
-					background: 'rgba(255,255,255,0.02)',
-				}}
-			>
-				<div className='flex items-center gap-3'>
-					<div style={{ padding: 10, background: `rgba(${accentRgb}, 0.12)`, color: accentColor, display: 'flex', flexShrink: 0 }}>
-						{icon}
-					</div>
-					<span style={{ fontWeight: 700, letterSpacing: '0.1em', fontSize: '0.85rem' }}>
-						{title.toUpperCase()}
-					</span>
-				</div>
-
-				<p style={{ fontSize: '0.8rem', color: 'rgba(237,237,237,0.45)', margin: 0 }}>
-					{description}
-				</p>
-
-				<div className='flex items-center gap-2 mt-auto' style={{ color: accentColor, fontSize: '0.8rem', fontWeight: 500, wordBreak: 'break-all' }}>
-					<span>{label}</span>
-					{external && <Launch style={{ fontSize: 14, flexShrink: 0 }} />}
-				</div>
-			</div>
+		<Link
+			href={href as any}
+			target={external ? '_blank' : '_self'}
+			className={s.channel}
+			style={{ '--acc': accent } as React.CSSProperties}
+		>
+			<div className={s.channelH}><i>{mark}</i><b>{title}</b></div>
+			<p>{description}</p>
+			<span className={s.channelV}>{label}</span>
 		</Link>
 	)
 }
 
-
 export default function Tab() {
 	return (
-		<div className='flex flex-col gap-5'>
+		<>
+		<section>
+			<SectionHead kicker='Get in touch' title='Contact us' />
 
-			<div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
-				<ContactCard
-					icon={<HeadsetMic />}
-					title="TeamSpeak"
-					accentColor="#00bcd4"
-					accentRgb="0,188,212"
-					href="ts3server://ts.asotmilsim.com"
-					label="ts.asotmilsim.com"
-					description="Join our TeamSpeak server for real-time voice communications during operations."
+			<div className={s.channels}>
+				<Channel
+					mark='TS'
+					title='TeamSpeak'
+					accent='#00bcd4'
+					href='ts3server://ts.asotmilsim.com'
+					label='ts.asotmilsim.com'
+					description='Join our TeamSpeak server for real-time voice communications during operations.'
 				/>
-				<ContactCard
-					icon={<Groups />}
-					title="Facebook"
-					accentColor="#1877F2"
-					accentRgb="24,119,242"
-					href="https://www.facebook.com/AustralianSpecialOperationsTaskforce"
-					label="AustralianSpecialOperationsTaskforce"
-					description="Follow us on Facebook for updates, event announcements, and community news."
+				<Channel
+					mark='f'
+					title='Facebook'
+					accent='#1877F2'
+					href='https://www.facebook.com/AustralianSpecialOperationsTaskforce'
+					label='AustralianSpecialOperationsTaskforce'
+					description='Follow us on Facebook for updates, event announcements, and community news.'
 					external
 				/>
-				<ContactCard
-					icon={<EmailIcon />}
-					title="Email"
-					accentColor="rgb(219,0,29)"
-					accentRgb="219,0,29"
-					href="mailto:australianspecialoperationstaskforce@hotmail.com"
-					label="australianspecialoperationstaskforce@hotmail.com"
-					description="Send us a direct message for any inquiries, applications, or general questions."
+				<Channel
+					mark='@'
+					title='Email'
+					accent='rgb(219,0,29)'
+					href='mailto:australianspecialoperationstaskforce@hotmail.com'
+					label='australianspecialoperationstaskforce@hotmail.com'
+					description='Send us a direct message for any inquiries, applications, or general questions.'
 				/>
 			</div>
+		</section>
 
-			<div
-				className='flex flex-col gap-4 p-5'
-				style={{
-					border: '1px solid rgba(88,101,242,0.25)',
-					borderTop: '2px solid #5865F2',
-					background: 'rgba(88,101,242,0.04)',
-				}}
-			>
-				<div className='flex items-center gap-3'>
-					<div style={{ padding: 10, background: 'rgba(88,101,242,0.15)', color: '#5865F2', display: 'flex' }}>
-						<Forum />
-					</div>
-					<div>
-						<div style={{ fontWeight: 700, letterSpacing: '0.1em', fontSize: '0.85rem' }}>DISCORD</div>
-						<div style={{ fontSize: '0.8rem', color: 'rgba(237,237,237,0.45)', marginTop: 2 }}>
-							Our primary community hub — join to connect with members, ask questions, and stay up to date.
-						</div>
-					</div>
+		<section className={s.widget}>
+			<div className={s.channelH}>
+				<i style={{ color: 'var(--discord)' }}>D</i>
+				<div>
+					<b>Discord</b>
+					<p style={{ marginTop: 2 }}>Our primary community hub — join to connect with members, ask questions, and stay up to date.</p>
 				</div>
-				<iframe
-					className='w-full'
-					style={{ height: 500, border: 'none' }}
-					src='https://discord.com/widget?id=744518510092484660&theme=dark'
-				/>
 			</div>
-
-		</div>
+			<iframe
+				title='ASOT Discord'
+				src='https://discord.com/widget?id=744518510092484660&theme=dark'
+			/>
+		</section>
+		</>
 	)
 }
